5ad225246256260b0311bd0d7799305353de4080
ozbuganliramazan <ozbuganliramazan@outlook.com>
2023-07-04 15:38:02 +0300

    arranging the menu

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.querySelector('.IconLeft .fa-sharp').addEventListener('click', function
     document.querySelector('.vertical-header').style.display = 'flex';
   });
   document.querySelector('.vertical-header .fa-xmark').addEventListener('click', function () {
+    console.log("click");
     document.querySelector('.vertical-header').style.display = 'none';
   });  
  
